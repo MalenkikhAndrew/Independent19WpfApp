304bc47d072e9e6e6acd6a899db0f3f3504874c8
Андрей Маленьких <mrmaloy-1992@mail.ru>
2022-12-01 10:41:19 +1000

    Добавьте файлы проекта.

---
+++ Independent19WpfApp/Models/Ariph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Independent19WpfApp.Models
+{
+    internal class Ariph
+    {
+        
+        public static double  Add(double a) => a * 2*Math.PI;
+    }
+}
